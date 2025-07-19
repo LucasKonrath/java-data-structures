8216bf20cc79dd1963665b983050d97d6ed84ca8
LucasKonrath <lucaskdamaceno@gmail.com>
2025-07-19 11:20:22 -0300

    Add CountingSort implementation

---
+++ src/main/java/sorting/CountingSort.java
@@ -0,0 +1,28 @@
+package sorting;
+
+public class CountingSort {
+    public void countingSort(int[] arr) {
+        if (arr.length == 0) return;
+
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+         int[] count = new int[max + 1];
+
+        for (int num : arr) {
+            count[num]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[index++] = i;
+                count[i]--;
+            }
+        }
+    }
+}
